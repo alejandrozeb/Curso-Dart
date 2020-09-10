840a1d90e9ce7bfef36fa5d91aafa3ca9b1538ca
unknown <alejandrojzeballos@gmail.com>
2020-09-09 21:55:58 -0400

    añadiendo introduccion a Dart

---
+++ Introduccion/1.Apuntes.dart
@@ -0,0 +1,20 @@
+/* 
+Dart es un lenguaje multiplataforma que ha sido diseñado para ser parte de cualquier proyecto y multilenguaje (IOS, Android)
+desarrolado por google, que permite desarrollar el backen, frotend y servicios.
+Flutter nos permite desarrollar codigo dart para movil y exportarlo a codigo nativo como ios y android. 
+Dart WEB se puede traducir a aplicaciones angular.
+server de misma forma.
+nos permite desarrollar aplicaciones en cualquier ambiente de desarrollo.
+
+Editores:
+Tenemos diferentes opciones:
+DARTPAD: es un editor del dart en su pagina.
+Herramientas: 
+VS con el plugin de dart ademas de flutter.
+Android Studio, igualmente con plugin.
+
+Jetbrains tambien que es el id de android studio.
+
+Instalacion en visual estudio Code
+debes ir a extensiones y descargar la extension flutter de parte de dartcode y ya podemos escribir codigo dart.
+ */
